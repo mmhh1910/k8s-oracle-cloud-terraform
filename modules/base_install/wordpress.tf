d67f7822a0b6263418a35795a9a3f744e273ddfa
mmhh1910 <mamoennig@gmail.com>
2023-08-25 09:38:29 +0200

    cleanup

--- modules/base_install/wordpress.tf
+++ modules/base_install/wordpress.tf
@@ -1,8 +1,3 @@
-# resource "oci_core_public_ip" "wp_ip" {
-#     compartment_id = var.compartment_id
-#     lifetime       = "RESERVED"
-#     display_name = "k8s_wordpress_public_ip"
-#   }
 
 
 # resource "helm_release" "wordpress" {
